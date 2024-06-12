37769fe1944f7400253df7cd11037e5bebca1c77
xinjian <xinjian.wang>
2024-06-12 10:00:01 -0400

    using wandb to visualize the training results

--- tutorials/hello_model.py
+++ tutorials/hello_model.py
@@ -1,30 +1,37 @@
 # main.py
-# ! pip install torchvision
-import torch, torch.nn as nn, torch.utils.data as data, torchvision as tv, torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torchvision as tv
+import torch.nn.functional as F
 import lightning as L
-from lightning.pytorch.loggers import TensorBoardLogger
-from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks import ModelCheckpoint, DeviceStatsMonitor
+from lightning.pytorch.profilers import AdvancedProfiler
 
 # --------------------------------
 # Step 1: Define a LightningModule
 # --------------------------------
-# A LightningModule (nn.Module subclass) defines a full *system*
-# (ie: an LLM, diffusion model, autoencoder, or simple image classifier).
-
 
 class LitAutoEncoder(L.LightningModule):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(nn.Linear(28 * 28, 128), nn.ReLU(), nn.Linear(128, 3))
         self.decoder = nn.Sequential(nn.Linear(3, 128), nn.ReLU(), nn.Linear(128, 28 * 28))
+        
+        #################### Save hyperparameters ####################
+        # self.save_hyperparameters()
+        # call this to save only (layer_1_dim=128) to the checkpoint
+        # self.save_hyperparameters("layer_1_dim")
+
+        # equivalent
+        # self.save_hyperparameters(ignore=["loss_fx", "generator_network"])
 
     def forward(self, x):
-        # in lightning, forward defines the prediction/inference actions
         embedding = self.encoder(x)
         return embedding
 
     def training_step(self, batch, batch_idx):
-        # training_step defines the train loop. It is independent of forward
         x, _ = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
@@ -34,22 +41,30 @@ class LitAutoEncoder(L.LightningModule):
         return loss
     
     def validation_step(self, batch, batch_idx):
+        self._shared_eval(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        self._shared_eval(batch, batch_idx, "test")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+    
+    def _shared_eval(self, batch, batch_idx, prefix):
         x, _ = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
         x_hat = self.decoder(z)
         loss = F.mse_loss(x_hat, x)
-        self.log("val_loss", loss)
+        self.log(f"{prefix}_loss", loss)
         return loss
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+# Initialize WandbLogger
+logger = WandbLogger(project="mnist_autoencoder", name="mnist_autoencoder")
 
-logger = TensorBoardLogger("lightning_logs", name="minist_autoencoder")
-
+# Initialize checkpoint callback
 checkpoint_callback = ModelCheckpoint(
-    monitor='train_loss',
+    monitor='val_loss',
     dirpath='checkpoints/',
     filename='model-{epoch:02d}-{val_loss:.2f}',
     save_top_k=3,
@@ -60,7 +75,6 @@ checkpoint_callback = ModelCheckpoint(
 # Step 2: Define data
 # -------------------
 torch.set_float32_matmul_precision('medium')
-# https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
 dataset = tv.datasets.MNIST(".", download=True, transform=tv.transforms.ToTensor())
 train, val = data.random_split(dataset, [55000, 5000])
 train_loader = data.DataLoader(train, batch_size=32, num_workers=19)
@@ -69,8 +83,16 @@ val_loader = data.DataLoader(val, batch_size=32, num_workers=19)
 # -------------------
 # Step 3: Train
 # -------------------
+profiler = AdvancedProfiler(dirpath=".", filename="perf_logs")
 autoencoder = LitAutoEncoder()
-trainer = L.Trainer(logger=logger, callbacks=[checkpoint_callback], max_epochs=5)
+trainer = L.Trainer(logger=logger, 
+                    callbacks=[checkpoint_callback], 
+                    max_epochs=5, 
+                    # profiler=profiler,
+                    profiler='simple',
+                    num_sanity_val_steps=0)
 trainer.fit(autoencoder, train_loader, val_loader)
 
-# tensorboard --logdir lightning_logs
+# To visualize logs and checkpoints with Wandb, run the following in your terminal:
+# wandb login
+# wandb sync --include-offline
