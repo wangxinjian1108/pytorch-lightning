ab18ac827e2366afa30bbf0df7f15ede97711ef4
xinjian <xinjian.wang>
2025-03-07 03:05:51 -0500

    [e2e 24] add fpn feature extractor

--- examples/pytorch/e2evision/models/__init__.py
+++ examples/pytorch/e2evision/models/__init__.py
@@ -1,14 +1,16 @@
 from .components import (
-    ImageFeatureExtractor,
     TrajectoryQueryRefineLayer,
     TrajectoryDecoder
 )
 from .network import E2EPerceptionNet
 from .module import E2EPerceptionModule
 from .temporal_fusion_layer import TemporalFusionFactory
+from .image_feature_extractor import FPNImageFeatureExtractor, FPNImageFeatureExtractorWithHooks, ImageFeatureExtractor
 
 __all__ = [
     'ImageFeatureExtractor',
+    'FPNImageFeatureExtractor',
+    'FPNImageFeatureExtractorWithHooks',
     'TrajectoryQueryRefineLayer',
     'TrajectoryDecoder',
     'E2EPerceptionNet',
