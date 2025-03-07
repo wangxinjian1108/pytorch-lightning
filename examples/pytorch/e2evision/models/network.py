d3f51c6d2b49ed49da10745779a569747133f8e1
xinjian <xinjian.wang>
2025-03-07 03:05:51 -0500

    [e2e 24] add fpn feature extractor

--- examples/pytorch/e2evision/models/network.py
+++ examples/pytorch/e2evision/models/network.py
@@ -4,8 +4,9 @@ from typing import Dict, List, Optional
 import numpy as np
 
 from base import SourceCameraId, TrajParamIndex, ObjectType, CameraParamIndex, EgoStateIndex
-from .components import ImageFeatureExtractor, TrajectoryDecoder
+from .components import TrajectoryDecoder
 from .temporal_fusion_layer import TemporalFusionFactory
+from .image_feature_extractor import ImageFeatureExtractor
 
     
 class E2EPerceptionNet(nn.Module):
@@ -23,7 +24,7 @@ class E2EPerceptionNet(nn.Module):
         
         # Create feature extractors for each camera
         self.feature_extractors = nn.ModuleDict({
-            str(camera_id.value): ImageFeatureExtractor(feature_dim, use_pretrained=use_pretrained, backbone=backbone)
+            str(camera_id.value): ImageFeatureExtractor([camera_id], feature_dim, use_pretrained=use_pretrained, backbone=backbone)
             for camera_id in camera_ids
         })
         
@@ -57,15 +58,11 @@ class E2EPerceptionNet(nn.Module):
             images = batch['images'][camera_id]  # [B, T, C, H, W]
             B, T, C, H, W = images.shape
             
-            # Process each timestep
-            features = []
-            for t in range(T):
-                feat = self.feature_extractors[str(camera_id.value)](images[:, t])
-                features.append(feat)
-            
-            # Stack temporal features
-            features = torch.stack(features, dim=1)  # [B, T, C, H, W]
-            all_features_dict[camera_id] = features
+            images = images.view(B * T, C, H, W)
+            feat = self.feature_extractors[str(camera_id.value)](images)
+            _, C0, H0, W0 = feat.shape
+            feat = feat.view(B, T, C0, H0, W0)
+            all_features_dict[camera_id] = feat
         
         # Fuse temporal features for each camera
         fused_features_dict = {}
