[e2e 30] fix binary entropy calculation bug, needs the logits

examples/pytorch/e2evision/models/components.py
@@ -67,11 +67,16 @@ class CrossAttentionTrajHead(nn.Module):
         self.attention = nn.MultiheadAttention(embed_dim=query_dim, num_heads=num_heads)
         
         # Prediction head
-        self.traj_head = nn.Sequential(
+        self.motion_head = nn.Sequential(
             nn.Linear(query_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, TrajParamIndex.END_OF_INDEX),
+            nn.Linear(hidden_dim, TrajParamIndex.HEIGHT + 1),
             nn.Sigmoid()
+        )
+        self.cls_head = nn.Sequential(
+            nn.Linear(query_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, TrajParamIndex.END_OF_INDEX - TrajParamIndex.HEIGHT - 1)
         )
         
     def forward(self, queries):
@@ -88,7 +93,11 @@ class CrossAttentionTrajHead(nn.Module):
         pooled_output = attn_output.mean(dim=0)  # Shape: (batch_size, query_dim)
         
         # Pass the result through the trajectory head for prediction
-        prediction = self.traj_head(pooled_output)  # Shape: (batch_size, END_OF_INDEX)
+        motion_params = self.motion_head(pooled_output)  # Shape: (batch_size, TrajParamIndex.HEIGHT + 1)
+        cls_params = self.cls_head(pooled_output)  # Shape: (batch_size, TrajParamIndex.END_OF_INDEX - TrajParamIndex.HEIGHT - 1)
+        
+        # Combine motion and cls parameters
+        prediction = torch.cat([motion_params, cls_params], dim=-1)  # Shape: (batch_size, TrajParamIndex.END_OF_INDEX)
         
         return prediction
     
@@ -119,12 +128,17 @@ class TrajectoryDecoder(nn.Module):
         self.register_buffer('motion_min_vals', ranges[:, 0])
         self.register_buffer('motion_ranges', ranges[:, 1] - ranges[:, 0])
          
-        # Single trajectory parameter head that outputs all trajectory parameters
-        self.traj_head = nn.Sequential(
+        # Single trajectory parameter head that outputs all trajectory parameters        
+        self.motion_head = nn.Sequential(
             nn.Linear(query_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, TrajParamIndex.END_OF_INDEX),
+            nn.Linear(hidden_dim, TrajParamIndex.HEIGHT + 1),
             nn.Sigmoid()
+        )
+        self.cls_head = nn.Sequential(
+            nn.Linear(query_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, TrajParamIndex.END_OF_INDEX - TrajParamIndex.HEIGHT - 1)
         )
         
         self.feature_mlp = nn.Sequential(
@@ -150,16 +164,12 @@ class TrajectoryDecoder(nn.Module):
     def decode_trajectory(self, x: torch.Tensor) -> torch.Tensor:
         """ Decode trajectory from features."""
         # 原始前向计算
-        traj_params = self.traj_head(x)
+        motion_params = self.motion_head(x)
+        motion_params = motion_params * self.motion_ranges + self.motion_min_vals
+        cls_params = self.cls_head(x)
         
-        # 创建新张量，避免原地操作
-        scaled_part = traj_params[..., :TrajParamIndex.HEIGHT + 1] * self.motion_ranges + self.motion_min_vals
-        unchanged_part = traj_params[..., TrajParamIndex.HEIGHT + 1:]
-        
-        # 合并两部分
-        new_traj_params = torch.cat([scaled_part, unchanged_part], dim=-1)
-        
-        return new_traj_params
+        traj_params = torch.cat([motion_params, cls_params], dim=-1)
+        return traj_params
     
     def forward(self, 
                 features_dict: Dict[SourceCameraId, torch.Tensor],

examples/pytorch/e2evision/predict.py
@@ -47,16 +47,17 @@ class PredictionWriter(L.Callback):
                 batch_traj_vecs = last_layer_outputs[i]
                 sorted_indices = torch.argsort(batch_traj_vecs[:, TrajParamIndex.HAS_OBJECT], descending=True)
                 batch_traj_vecs = batch_traj_vecs[sorted_indices]
-                print(f"\n query idx {sorted_indices[0]} has largest confidence of having object: {batch_traj_vecs[0][TrajParamIndex.HAS_OBJECT]:.2f},"
+                print(f"\n query idx {sorted_indices[0]} has largest confidence of having object: {torch.sigmoid(batch_traj_vecs[0][TrajParamIndex.HAS_OBJECT]):.2f},"
                       f"\n position: {batch_traj_vecs[0][TrajParamIndex.X]:.2f}, {batch_traj_vecs[0][TrajParamIndex.Y]:.2f}, {batch_traj_vecs[0][TrajParamIndex.Z]:.2f}"
                       f"\n dimension: {batch_traj_vecs[0][TrajParamIndex.LENGTH]:.2f}, {batch_traj_vecs[0][TrajParamIndex.WIDTH]:.2f}, {batch_traj_vecs[0][TrajParamIndex.HEIGHT]:.2f}"
                       f"\n velocity: {batch_traj_vecs[0][TrajParamIndex.VX]:.2f}, {batch_traj_vecs[0][TrajParamIndex.VY]:.2f},"
                       f"\n acceleration: {batch_traj_vecs[0][TrajParamIndex.AX]:.3f}, {batch_traj_vecs[0][TrajParamIndex.AY]:.3f}")
+                import pdb; pdb.set_trace()
                 
             for j in range(last_layer_outputs.shape[1]):
                 traj_vec = last_layer_outputs[i][j]
-                if traj_vec[TrajParamIndex.HAS_OBJECT] > 0.5:
-                    valid_trajs.append(tensor_to_trajectory(traj_vec))
+                if torch.sigmoid(traj_vec[TrajParamIndex.HAS_OBJECT]) > 0.5:
+                    valid_trajs.append(tensor_to_trajectory(traj_vec).to_dict)
         
         print(f"This batch has {len(valid_trajs)} valid predictions")
         